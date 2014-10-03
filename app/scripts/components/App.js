29f5ef16ad598aff8a48f4c040de09d875a4700a
Tanoy Sinha <tsinha@gmail.com>
2014-10-03 13:39:38 -0400

    topbar

--- app/scripts/components/App.js
+++ app/scripts/components/App.js
@@ -4,18 +4,31 @@
 
 'use strict';
 
-var React       = require('react/addons'),
-    r$          = require('reqwest'),
-    TopBar      = require('./TopBar'),
-    Dropdown    = require('./Dropdown'),
-    Form        = require('./Form');
+var React           = require('react/addons'),
+    r$              = require('reqwest'),
+    Dropdown        = require('./Dropdown'),
+    Navbar          = require('react-bootstrap/Navbar'),
+    Nav             = require('react-bootstrap/Nav'),
+    NavItem         = require('react-bootstrap/NavItem'),
+    MenuItem        = require('react-bootstrap/MenuItem'),
+    DropdownButton  = require('react-bootstrap/DropdownButton'),
+    Form            = require('./Form');
 
 var App = React.createClass({
 
   render: function() {
     return (
       <div id='app'>
-        <TopBar />
+        <Navbar fluid staticTop>
+          <Nav>
+            <NavItem key={1} href="#">Post a Game</NavItem>
+            <DropdownButton key={3} title="View Matches">
+              <MenuItem key="1">Unconfirmed Matches</MenuItem>
+              <MenuItem key="2">Recently Confirmed</MenuItem>
+            </DropdownButton>
+            <NavItem key={2} href="#">Login</NavItem>
+          </Nav>
+        </Navbar>
         <Form>
         </Form>
       </div>
