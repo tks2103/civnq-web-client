a99c0bc50008b310b2fce0961ec7b13df448ff0d
Tanoy Sinha <tsinha@gmail.com>
2014-10-07 12:06:02 -0400

    confirmed, unconfirmed, home, nav

--- app/scripts/components/App.js
+++ app/scripts/components/App.js
@@ -4,38 +4,61 @@
 
 'use strict';
 
-var React           = require('react/addons'),
-    r$              = require('reqwest'),
-    Header          = require('./Header'),
-    Rankings        = require('./Rankings'),
-    Body            = require('./Body'),
-    PostMatch       = require('./PostMatch'),
-    Grid            = require('react-bootstrap/Grid'),
-    Row             = require('react-bootstrap/Row'),
-    Col             = require('react-bootstrap/Col'),
-    Form            = require('./Form');
+var React               = require('react/addons'),
+    r$                  = require('reqwest'),
+    Header              = require('./Header'),
+    Rankings            = require('./Rankings'),
+    Body                = require('./Body'),
+    UnconfirmedMatches  = require('./UnconfirmedMatches'),
+    ConfirmedMatches    = require('./ConfirmedMatches'),
+    PostMatch           = require('./PostMatch'),
+    Grid                = require('react-bootstrap/Grid'),
+    Row                 = require('react-bootstrap/Row'),
+    Col                 = require('react-bootstrap/Col'),
+    Form                = require('./Form');
 
 var App = React.createClass({
 
   getInitialState: function() {
-    return ( { modal: false } );
+    return ( { 
+      postMatchModal: false,
+      body: "Rankings"
+    } );
   },
 
 
   togglePostMatch: function() {
-    this.setState({ modal: !this.state.modal });
+    this.setState({ postMatchModal: !this.state.postMatchModal });
+  },
+
+
+  displayUnconfirmedMatches: function() {
+    this.setState({ body: "UnconfirmedMatches" });
+  },
+
+
+  displayConfirmedMatches: function() {
+    this.setState({ body: "ConfirmedMatches" });
+  },
+
+
+  displayRankings: function() { 
+    this.setState({ body: "Rankings" });
   },
 
 
   render: function() {
     return (
       <div id='app'>
-        {this.state.modal ? <PostMatch togglePostMatch={this.togglePostMatch} /> : null }
-        <Header togglePostMatch={this.togglePostMatch} />
+        {this.state.postMatchModal ? <PostMatch togglePostMatch={this.togglePostMatch} /> : null }
+        <Header togglePostMatch={this.togglePostMatch} 
+                displayConfirmedMatches={this.displayConfirmedMatches} 
+                displayUnconfirmedMatches={this.displayUnconfirmedMatches}
+                displayRankings={this.displayRankings} />
         <Grid>
-          <Body />
-          <Rankings />
-          <Rankings />
+          {this.state.body == "Rankings" ? <Body /> : null}
+          {this.state.body == "UnconfirmedMatches" ? <UnconfirmedMatches /> : null}
+          {this.state.body == "ConfirmedMatches" ? <ConfirmedMatches /> : null}
         </Grid>
         <Form />  
       </div>
---
+++ app/scripts/components/UnconfirmedMatches.js
@@ -0,0 +1,19 @@
+/**
+ * @jsx React.DOM
+ */
+
+'use strict';
+
+var React           = require('react/addons');
+
+var UnconfirmedMatches = React.createClass({
+
+  render: function() {
+    return (
+      <div></div>
+    );
+  }
+
+});
+
+module.exports = UnconfirmedMatches;
