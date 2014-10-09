804ef496d3ca9258dd18d210bb86ef1107abf8fd
AshleyGlidden <ashley@ashleyglidden.com>
2014-10-09 14:50:55 -0400

    view match data in unconfirmed matches

--- app/scripts/components/UnconfirmedMatches.js
+++ app/scripts/components/UnconfirmedMatches.js
@@ -8,14 +8,24 @@ var React           = require('react/addons'),
     Match           = require('./Match');
 
 var UnconfirmedMatches = React.createClass({
+  
+
+  getMatch: function() { 
+    var unconfirmedMatches = this.props.data.unconfirmed_matches;
+    var matchesToRender = unconfirmedMatches.map(function(i){
+      return (
+          <Match data={i}/>
+      );
+    });  
+    return matchesToRender;
+  },
 
   render: function() {
-    console.log(this.props.data.unconfirmed_matches);
+    var matches = this.getMatch();
+    console.log(matches);
     return (
       <div>
-        <Match />
-        <Match />
-        <Match />
+        {matches}
       </div>
     );
   }
